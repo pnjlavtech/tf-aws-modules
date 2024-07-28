9a4ecfbfa8321ad7c7396014405b2839cc9bebfd
Jay Lavine <jaylavine@gmail.com>
2024-07-28 15:25:49 -0700

    feat(s3): Creating s3 terraform module

---
+++ s3/outputs.tf
@@ -0,0 +1,14 @@
+output "arn" {
+  value       = aws_s3_bucket.this[0].arn
+  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
+}
+
+output "bucket_domain_name" {
+  value       = aws_s3_bucket.this[0].bucket_domain_name
+  description = "The bucket domain name. Will be of format bucketname.s3.amazonaws.com."
+}
+
+output "id" {
+  value       = aws_s3_bucket.this[0].id
+  description = "The name of the bucket."
+}
