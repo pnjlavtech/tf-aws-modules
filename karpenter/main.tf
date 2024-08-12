477ee50b25e8d9ce56cfd86b475a910ea7c9ec41
Jay Lavine <jaylavine@gmail.com>
2024-08-11 17:36:11 -0700

    Karpenter module, add crd resource add annotations

--- karpenter/main.tf
+++ karpenter/main.tf
@@ -90,15 +90,57 @@ module "karpenter_disabled" {
 # Not required; just to demonstrate functionality of the sub-module
 ################################################################################
 
+# resource "helm_release" "karpenter" {
+#   namespace           = "kube-system"
+#   name                = "karpenter"
+#   repository          = "oci://public.ecr.aws/karpenter"
+#   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+#   repository_password = data.aws_ecrpublic_authorization_token.token.password
+#   chart               = "karpenter"
+#   version             = "0.37.0"
+#   wait                = false
+
+#   values = [
+#     <<-EOT
+#     serviceAccount:
+#       name: ${module.karpenter.service_account}
+#     settings:
+#       clusterName: ${var.cluster_name}
+#       clusterEndpoint: ${var.cluster_endpoint}
+#       interruptionQueue: ${module.karpenter.queue_name}
+#     EOT
+#   ]
+# }
+
+# Check this for update notes first:
+# https://karpenter.sh/docs/upgrading/upgrade-guide/#crd-upgrades
+resource "helm_release" "karpenter-crd" {
+  namespace           = "karpenter"
+  create_namespace    = true
+  name                = "karpenter-crd"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter-crd"
+  version             = var.karpenter.chart_version
+  wait                = true
+  values              = []
+}
+
 resource "helm_release" "karpenter" {
-  namespace           = "kube-system"
+  depends_on          = [ helm_release.karpenter-crd ]
+  namespace           = "karpenter"
+  create_namespace    = true
   name                = "karpenter"
   repository          = "oci://public.ecr.aws/karpenter"
   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
   repository_password = data.aws_ecrpublic_authorization_token.token.password
   chart               = "karpenter"
-  version             = "0.37.0"
-  wait                = false
+  version             = var.karpenter.chart_version
+  wait                = true
+  
+  # The crds are controlled by the karpenter-crd chart above.
+  skip_crds = true
 
   values = [
     <<-EOT
@@ -112,12 +154,18 @@ resource "helm_release" "karpenter" {
   ]
 }
 
+
 resource "kubectl_manifest" "karpenter_node_class" {
   yaml_body = <<-YAML
     apiVersion: karpenter.k8s.aws/v1beta1
     kind: EC2NodeClass
     metadata:
       name: default
+      annotations:
+        meta.helm.sh/release-name: karpenter-crd
+        meta.helm.sh/release-namespace: karpenter
+      labels:
+        app.kubernetes.io/managed-by: Helm
     spec:
       amiFamily: AL2023
       role: ${module.karpenter.node_iam_role_name}
@@ -142,6 +190,11 @@ resource "kubectl_manifest" "karpenter_node_pool" {
     kind: NodePool
     metadata:
       name: default
+      annotations:
+        meta.helm.sh/release-name: karpenter-crd
+        meta.helm.sh/release-namespace: karpenter
+      labels:
+        app.kubernetes.io/managed-by: Helm
     spec:
       template:
         spec:
@@ -174,32 +227,32 @@ resource "kubectl_manifest" "karpenter_node_pool" {
 
 # Example deployment using the [pause image](https://www.ianlewis.org/en/almighty-pause-container)
 # and starts with zero replicas
-resource "kubectl_manifest" "karpenter_example_deployment" {
-  yaml_body = <<-YAML
-    apiVersion: apps/v1
-    kind: Deployment
-    metadata:
-      name: inflate
-    spec:
-      replicas: 0
-      selector:
-        matchLabels:
-          app: inflate
-      template:
-        metadata:
-          labels:
-            app: inflate
-        spec:
-          terminationGracePeriodSeconds: 0
-          containers:
-            - name: inflate
-              image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
-              resources:
-                requests:
-                  cpu: 1
-  YAML
+# resource "kubectl_manifest" "karpenter_example_deployment" {
+#   yaml_body = <<-YAML
+#     apiVersion: apps/v1
+#     kind: Deployment
+#     metadata:
+#       name: inflate
+#     spec:
+#       replicas: 0
+#       selector:
+#         matchLabels:
+#           app: inflate
+#       template:
+#         metadata:
+#           labels:
+#             app: inflate
+#         spec:
+#           terminationGracePeriodSeconds: 0
+#           containers:
+#             - name: inflate
+#               image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
+#               resources:
+#                 requests:
+#                   cpu: 1
+#   YAML
 
-  depends_on = [
-    helm_release.karpenter
-  ]
-}
+#   depends_on = [
+#     helm_release.karpenter
+#   ]
+# }
