d5d28ea97db064aa6d10b3bc68ca73e42d249872
Jay Lavine <jaylavine@gmail.com>
2024-08-24 14:34:05 -0700

    Karpenter module - change token ternary

--- karpenter/main.tf
+++ karpenter/main.tf
@@ -1,150 +1,108 @@
-# provider "aws" {
-#   region = "us-east-1"
-#   alias  = "virginia"
-# }
+provider "aws" {
+  region = "us-east-1"
+  alias  = "virginia"
+}
 
-# provider "aws" {
-#   region = "us-west-2"
-#   alias  = "oregon"
-# }
+provider "aws" {
+  region = "us-west-2"
+  alias  = "oregon"
+}
 
-#  terraform {
-#   required_version = ">= 1.3.2"
+ terraform {
+  required_version = ">= 1.3.2"
 
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = ">= 5.50"
-#     }
-#     helm = {
-#       source  = "hashicorp/helm"
-#       version = ">= 2.13.1"
-#     }
-#     kubectl = {
-#       source  = "alekc/kubectl"
-#       version = ">= 2.0"
-#     }
-#   }
-# }
-
-
-
-
-# # obtain a cluster token for providers, tokens are short lived (15 minutes)
-# # provider "kubernetes" {
-# #   host                   = data.aws_eks_cluster.eks_cluster_name.endpoint
-# #   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster_name.certificate_authority[0].data)
-# #   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-# # }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.50"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.13.1"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0"
+    }
+  }
+}
 
 
 
-# # obtain a cluster token for providers, tokens are short lived (15 minutes)
-# provider "kubectl" {
-#   apply_retry_count      = 5
-#   host                   = var.cluster_endpoint
-#   cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-#   token                  = var.cluster_auth_token == "" ? "" : data.aws_eks_cluster_auth.cluster_auth[0].token
-#   load_config_file       = false
-# }
 
-
-# # obtain a cluster token for providers, tokens are short lived (15 minutes)
-# provider "helm" {
-#   kubernetes {
-#     host                   = var.cluster_endpoint
-#     cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-#     token                  = var.cluster_auth_token == "" ? "" : data.aws_eks_cluster_auth.cluster_auth[0].token
-
-#   }
+# obtain a cluster token for providers, tokens are short lived (15 minutes)
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.eks_cluster_name.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster_name.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.cluster_auth.token
 # }
 
 
 
-# ################################################################################
-# # Karpenter
-# ################################################################################
+# obtain a cluster token for providers, tokens are short lived (15 minutes)
+provider "kubectl" {
+  apply_retry_count      = 5
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth[0].token != null ? data.aws_eks_cluster_auth.cluster_auth[0].token : ""
+  # token                  = var.cluster_auth_token == "" ? "" : data.aws_eks_cluster_auth.cluster_auth[0].token
+  load_config_file       = false
+}
 
-# module "karpenter" {
-#   source = "terraform-aws-modules/eks/aws//modules/karpenter"
 
-#   cluster_name = var.cluster_name
+# obtain a cluster token for providers, tokens are short lived (15 minutes)
+provider "helm" {
+  kubernetes {
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster_auth[0].token != null ? data.aws_eks_cluster_auth.cluster_auth[0].token : ""
+    # token                  = var.cluster_auth_token == "" ? "" : data.aws_eks_cluster_auth.cluster_auth[0].token
 
-#   enable_pod_identity             = true
-#   create_pod_identity_association = true
+  }
+}
 
-#   # Used to attach additional IAM policies to the Karpenter node IAM role
-#   node_iam_role_additional_policies = {
-#     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-#   }
 
-#   tags = var.tags
-# }
 
-# module "karpenter_disabled" {
-#   source = "terraform-aws-modules/eks/aws//modules/karpenter"
+################################################################################
+# Karpenter
+################################################################################
 
-#   create = false
-# }
+module "karpenter" {
+  source = "terraform-aws-modules/eks/aws//modules/karpenter"
 
-# ################################################################################
-# # Karpenter Helm chart & manifests
-# # Not required; just to demonstrate functionality of the sub-module
-# ################################################################################
+  cluster_name = var.cluster_name
 
-# # resource "helm_release" "karpenter" {
-# #   namespace           = "kube-system"
-# #   name                = "karpenter"
-# #   repository          = "oci://public.ecr.aws/karpenter"
-# #   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-# #   repository_password = data.aws_ecrpublic_authorization_token.token.password
-# #   chart               = "karpenter"
-# #   version             = "0.37.0"
-# #   wait                = false
+  enable_pod_identity             = true
+  create_pod_identity_association = true
 
-# #   values = [
-# #     <<-EOT
-# #     serviceAccount:
-# #       name: ${module.karpenter.service_account}
-# #     settings:
-# #       clusterName: ${var.cluster_name}
-# #       clusterEndpoint: ${var.cluster_endpoint}
-# #       interruptionQueue: ${module.karpenter.queue_name}
-# #     EOT
-# #   ]
-# # }
+  # Used to attach additional IAM policies to the Karpenter node IAM role
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
 
-# # Check this for update notes first:
-# # https://karpenter.sh/docs/upgrading/upgrade-guide/#crd-upgrades
-# resource "helm_release" "karpenter-crd" {
-#   namespace           = "kube-system"
-#   create_namespace    = false
-#   cleanup_on_fail     = true
-#   name                = "karpenter-crd"
-#   repository          = "oci://public.ecr.aws/karpenter"
-#   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-#   repository_password = data.aws_ecrpublic_authorization_token.token.password
-#   chart               = "karpenter-crd"
-#   version             = var.chart_version
-#   wait                = true
-#   values              = []
-# }
+  tags = var.tags
+}
+
+module "karpenter_disabled" {
+  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+
+  create = false
+}
+
+################################################################################
+# Karpenter Helm chart & manifests
+# Not required; just to demonstrate functionality of the sub-module
+################################################################################
 
 # resource "helm_release" "karpenter" {
-#   depends_on          = [ helm_release.karpenter-crd ]
 #   namespace           = "kube-system"
-#   create_namespace    = false
-#   cleanup_on_fail     = true
 #   name                = "karpenter"
 #   repository          = "oci://public.ecr.aws/karpenter"
 #   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
 #   repository_password = data.aws_ecrpublic_authorization_token.token.password
 #   chart               = "karpenter"
-#   version             = var.chart_version
-#   wait                = true
-  
-#   # The crds are controlled by the karpenter-crd chart above.
-#   skip_crds = true
+#   version             = "0.37.0"
+#   wait                = false
 
 #   values = [
 #     <<-EOT
@@ -158,74 +116,118 @@
 #   ]
 # }
 
+# Check this for update notes first:
+# https://karpenter.sh/docs/upgrading/upgrade-guide/#crd-upgrades
+resource "helm_release" "karpenter-crd" {
+  namespace           = "kube-system"
+  create_namespace    = false
+  cleanup_on_fail     = true
+  name                = "karpenter-crd"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter-crd"
+  version             = var.chart_version
+  wait                = true
+  values              = []
+}
 
-# resource "kubectl_manifest" "karpenter_node_class" {
-#   yaml_body = <<-YAML
-#     apiVersion: karpenter.k8s.aws/v1beta1
-#     kind: EC2NodeClass
-#     metadata:
-#       name: default
-#       annotations:
-#         meta.helm.sh/release-name: karpenter-crd
-#         meta.helm.sh/release-namespace: kube-system
-#       labels:
-#         app.kubernetes.io/managed-by: Helm
-#     spec:
-#       amiFamily: AL2023
-#       role: ${module.karpenter.node_iam_role_name}
-#       subnetSelectorTerms:
-#         - tags:
-#             karpenter.sh/discovery: ${var.cluster_name}
-#       securityGroupSelectorTerms:
-#         - tags:
-#             karpenter.sh/discovery: ${var.cluster_name}
-#       tags:
-#         karpenter.sh/discovery: ${var.cluster_name}
-#   YAML
+resource "helm_release" "karpenter" {
+  depends_on          = [ helm_release.karpenter-crd ]
+  namespace           = "kube-system"
+  create_namespace    = false
+  cleanup_on_fail     = true
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter"
+  version             = var.chart_version
+  wait                = false
+  
+  # The crds are controlled by the karpenter-crd chart above.
+  skip_crds = true
 
-#   depends_on = [
-#     helm_release.karpenter
-#   ]
-# }
+  values = [
+    <<-EOT
+    serviceAccount:
+      name: ${module.karpenter.service_account}
+    settings:
+      clusterName: ${var.cluster_name}
+      clusterEndpoint: ${var.cluster_endpoint}
+      interruptionQueue: ${module.karpenter.queue_name}
+    EOT
+  ]
+}
 
 
-# resource "kubectl_manifest" "karpenter_node_pool" {
-#   yaml_body = <<-YAML
-#     apiVersion: karpenter.sh/v1beta1
-#     kind: NodePool
-#     metadata:
-#       name: default
-#       annotations:
-#         meta.helm.sh/release-name: karpenter-crd
-#         meta.helm.sh/release-namespace: kube-system
-#       labels:
-#         app.kubernetes.io/managed-by: Helm
-#     spec:
-#       template:
-#         spec:
-#           nodeClassRef:
-#             name: default
-#           requirements:
-#             - key: "karpenter.k8s.aws/instance-category"
-#               operator: In
-#               values: ["c", "m", "r"]
-#             - key: "karpenter.k8s.aws/instance-cpu"
-#               operator: In
-#               values: ["4", "8", "16", "32"]
-#             - key: "karpenter.k8s.aws/instance-hypervisor"
-#               operator: In
-#               values: ["nitro"]
-#             - key: "karpenter.k8s.aws/instance-generation"
-#               operator: Gt
-#               values: ["2"]
-#       limits:
-#         cpu: 1000
-#       disruption:
-#         consolidationPolicy: WhenEmpty
-#         consolidateAfter: 30s
-#   YAML
+resource "kubectl_manifest" "karpenter_node_class" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1beta1
+    kind: EC2NodeClass
+    metadata:
+      name: default
+      annotations:
+        meta.helm.sh/release-name: karpenter-crd
+        meta.helm.sh/release-namespace: kube-system
+      labels:
+        app.kubernetes.io/managed-by: Helm
+    spec:
+      amiFamily: AL2023
+      role: ${module.karpenter.node_iam_role_name}
+      subnetSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${var.cluster_name}
+      securityGroupSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${var.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${var.cluster_name}
+  YAML
 
-#   depends_on = [
-#     kubectl_manifest.karpenter_node_class
-#   ]
-# }
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+
+resource "kubectl_manifest" "karpenter_node_pool" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.sh/v1beta1
+    kind: NodePool
+    metadata:
+      name: default
+      annotations:
+        meta.helm.sh/release-name: karpenter-crd
+        meta.helm.sh/release-namespace: kube-system
+      labels:
+        app.kubernetes.io/managed-by: Helm
+    spec:
+      template:
+        spec:
+          nodeClassRef:
+            name: default
+          requirements:
+            - key: "karpenter.k8s.aws/instance-category"
+              operator: In
+              values: ["c", "m", "r"]
+            - key: "karpenter.k8s.aws/instance-cpu"
+              operator: In
+              values: ["4", "8", "16", "32"]
+            - key: "karpenter.k8s.aws/instance-hypervisor"
+              operator: In
+              values: ["nitro"]
+            - key: "karpenter.k8s.aws/instance-generation"
+              operator: Gt
+              values: ["2"]
+      limits:
+        cpu: 1000
+      disruption:
+        consolidationPolicy: WhenEmpty
+        consolidateAfter: 30s
+  YAML
+
+  depends_on = [
+    kubectl_manifest.karpenter_node_class
+  ]
+}
