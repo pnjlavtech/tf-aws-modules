a5e78b07a826576fb761b42fc971e5bb1be2d2e8
Jay Lavine <jaylavine@gmail.com>
2024-08-25 13:58:30 -0700

    EKS Module - slight upd, tags

--- eks/main.tf
+++ eks/main.tf
@@ -16,48 +16,18 @@ provider "aws" {
       source  = "hashicorp/aws"
       version = ">= 5.50"
     }
-    # helm = {
-    #   source  = "hashicorp/helm"
-    #   version = ">= 2.13.1"
-    # }
-    # kubectl = {
-    #   source  = "alekc/kubectl"
-    #   version = ">= 2.0"
-    # }
   }
 }
 
 
 
-
-# obtain a cluster token for providers, tokens are short lived (15 minutes)
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.eks_cluster_name.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster_name.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-# }
-
-
-# provider "kubectl" {
-#   apply_retry_count      = 5
-#   host                   = data.aws_eks_cluster.eks_cluster_name.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster_name.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-#   load_config_file       = false
-# }
-
-
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.aws_eks_cluster.eks_cluster_name.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster_name.certificate_authority[0].data)
-#     token                  = data.aws_eks_cluster_auth.cluster_auth.token
-
-#   }
-# }
-
-
+locals {
+  tags = {
+    Clustername = var.eks_fname
+    GithubRepo  = "tf-aws-modules"
+    module      = "eks"
+  }
+}
 
 
 
@@ -109,7 +79,9 @@ module "eks" {
     }
   }
 
-  tags = merge(var.tags, {
+  
+
+  tags = merge(local.tags, {
     # NOTE - if creating multiple security groups with this module, only tag the
     # security group that Karpenter should utilize with the following tag
     # (i.e. - at most, only one security group should have this tag in your account)
@@ -118,147 +90,3 @@ module "eks" {
 }
 
 
-################################################################################
-# Karpenter
-################################################################################
-
-# module "karpenter" {
-#   source = "terraform-aws-modules/eks/aws//modules/karpenter"
-
-#   cluster_name = module.eks.cluster_name
-
-#   enable_pod_identity             = true
-#   create_pod_identity_association = true
-
-#   # Used to attach additional IAM policies to the Karpenter node IAM role
-#   node_iam_role_additional_policies = {
-#     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-#   }
-
-#   tags = var.tags
-# }
-
-# module "karpenter_disabled" {
-#   source = "terraform-aws-modules/eks/aws//modules/karpenter"
-
-#   create = false
-# }
-
-# ################################################################################
-# # Karpenter Helm chart & manifests
-# # Not required; just to demonstrate functionality of the sub-module
-# ################################################################################
-
-# resource "helm_release" "karpenter" {
-#   namespace           = "kube-system"
-#   name                = "karpenter"
-#   repository          = "oci://public.ecr.aws/karpenter"
-#   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-#   repository_password = data.aws_ecrpublic_authorization_token.token.password
-#   chart               = "karpenter"
-#   version             = "0.37.0"
-#   wait                = false
-
-#   values = [
-#     <<-EOT
-#     serviceAccount:
-#       name: ${module.karpenter.service_account}
-#     settings:
-#       clusterName: ${module.eks.cluster_name}
-#       clusterEndpoint: ${module.eks.cluster_endpoint}
-#       interruptionQueue: ${module.karpenter.queue_name}
-#     EOT
-#   ]
-# }
-
-# resource "kubectl_manifest" "karpenter_node_class" {
-#   yaml_body = <<-YAML
-#     apiVersion: karpenter.k8s.aws/v1beta1
-#     kind: EC2NodeClass
-#     metadata:
-#       name: default
-#     spec:
-#       amiFamily: AL2023
-#       role: ${module.karpenter.node_iam_role_name}
-#       subnetSelectorTerms:
-#         - tags:
-#             karpenter.sh/discovery: ${module.eks.cluster_name}
-#       securityGroupSelectorTerms:
-#         - tags:
-#             karpenter.sh/discovery: ${module.eks.cluster_name}
-#       tags:
-#         karpenter.sh/discovery: ${module.eks.cluster_name}
-#   YAML
-
-#   depends_on = [
-#     helm_release.karpenter
-#   ]
-# }
-
-# resource "kubectl_manifest" "karpenter_node_pool" {
-#   yaml_body = <<-YAML
-#     apiVersion: karpenter.sh/v1beta1
-#     kind: NodePool
-#     metadata:
-#       name: default
-#     spec:
-#       template:
-#         spec:
-#           nodeClassRef:
-#             name: default
-#           requirements:
-#             - key: "karpenter.k8s.aws/instance-category"
-#               operator: In
-#               values: ["c", "m", "r"]
-#             - key: "karpenter.k8s.aws/instance-cpu"
-#               operator: In
-#               values: ["4", "8", "16", "32"]
-#             - key: "karpenter.k8s.aws/instance-hypervisor"
-#               operator: In
-#               values: ["nitro"]
-#             - key: "karpenter.k8s.aws/instance-generation"
-#               operator: Gt
-#               values: ["2"]
-#       limits:
-#         cpu: 1000
-#       disruption:
-#         consolidationPolicy: WhenEmpty
-#         consolidateAfter: 30s
-#   YAML
-
-#   depends_on = [
-#     kubectl_manifest.karpenter_node_class
-#   ]
-# }
-
-# # Example deployment using the [pause image](https://www.ianlewis.org/en/almighty-pause-container)
-# # and starts with zero replicas
-# resource "kubectl_manifest" "karpenter_example_deployment" {
-#   yaml_body = <<-YAML
-#     apiVersion: apps/v1
-#     kind: Deployment
-#     metadata:
-#       name: inflate
-#     spec:
-#       replicas: 0
-#       selector:
-#         matchLabels:
-#           app: inflate
-#       template:
-#         metadata:
-#           labels:
-#             app: inflate
-#         spec:
-#           terminationGracePeriodSeconds: 0
-#           containers:
-#             - name: inflate
-#               image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
-#               resources:
-#                 requests:
-#                   cpu: 1
-#   YAML
-
-#   depends_on = [
-#     helm_release.karpenter
-#   ]
-# }
