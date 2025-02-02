0b0a393d129ecdf56fb1dc7656aad2e38bb5619d
Jay Lavine <jaylavine@gmail.com>
2025-02-02 12:05:27 -0800

    EKS-ALB module upd data and zone info

--- eks-alb/dependencies.tf
+++ eks-alb/dependencies.tf
@@ -3,20 +3,7 @@ data "aws_acm_certificate" "acm_certificate_argo" {
   domain = var.domain_name_argo
 }
 
-
-data "aws_route53_zone" "route53_zone" {
-  count = var.use_route53_zone ? 1 : 0
-  name  = var.route53_zone_name
-
-  private_zone = var.route53_zone_private
+data "aws_route53_zone" "this" {
+  name         = var.public_domain
+  private_zone = false
 }
-
-data "aws_route53_zone" "route53_zone_argocd" {
-  count = var.use_route53_zone ? 1 : 0
-  name  = var.route53_zone_name_argo
-
-  private_zone = var.route53_zone_private_argocd
-}
-
-
-
