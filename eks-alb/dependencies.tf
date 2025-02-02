65b5aad4fe026ab99057280c15f5d29e7d860dc0
Jay Lavine <jaylavine@gmail.com>
2025-02-02 13:50:03 -0800

    eks-alb-module trying to get certificate_arn correect

--- eks-alb/dependencies.tf
+++ eks-alb/dependencies.tf
@@ -1,8 +1,3 @@
-data "aws_acm_certificate" "acm_certificate_argo" {
-  count  = var.create_alb_for_argocd ? 1 : 0
-  domain = coalesce(local.public_domain, local.domain_name_argo)
-}
-
 data "aws_route53_zone" "this" {
   name         = var.public_domain
   private_zone = false
