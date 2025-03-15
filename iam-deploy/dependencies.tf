55570fc9a3855b5adfd68447a22da0129d0a1d9f
Jay Lavine <jaylavine@gmail.com>
2025-03-15 14:18:04 -0700

    upd aud test to StringLike from StringEquals

--- iam-deploy/dependencies.tf
+++ iam-deploy/dependencies.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "github_oidc_role_policy" {
       identifiers = [data.aws_iam_openid_connect_provider.github.arn]
     }
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       values   = ["sts.amazonaws.com"]
       variable = "token.actions.githubusercontent.com:aud"
     }
